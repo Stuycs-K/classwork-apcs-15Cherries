1b7e254751eba452eb3d31fdf34676f2fc3a937b
Nowar <nowarnizum2008@icloud.com>
2024-09-29 20:28:24 -0400

    array to Strings method added

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,1 +1,18 @@
 
+public class MyArrays{
+  public static void main (String [] args){
+    System.out.println("Hello");
+  }
+
+public static String aryToStrings (int [] nums){
+  String newString = "[";
+  for (int i = 0; i < nums.length; i++){
+    newString = newString + nums[i];
+    if (i != nums.length - 1){
+      newString = newString + ",";
+    }
+  }
+  return newString + "[";
+}
+
+}
