e606eb67b092b3ccc4114ea734a3906df4f2d728
Nowar <nowarnizum2008@icloud.com>
2024-09-30 21:44:42 -0400

    indentation fixed

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -25,8 +25,6 @@ public class ArrayMethods{
     System.out.println(arrToString(swapRC(new int [] [] { {} })));                                //empty array
     System.out.println(arrToString(swapRC(new int [] [] { {2, 3, 4, 10}, {5, 6, 7, 7}, {2, 4, 9, 8}, {9, 4, 1, 0}}))); //sqaure array
     System.out.println(arrToString(swapRC(new int [] [] { {1, 4, 7, 10}, {3, 9, 12, 30}, {3, 6, 9, 8}, {2, 7, 5, 1}, {4, 1, 3, 8}})));
-
-
   }
 
   //2. Copy your arrToString method from before.
@@ -46,49 +44,49 @@ public class ArrayMethods{
   }
 
 
-//3. Write arrToString, with a 2D array parameter.
-//Note: Different parameters are allowed with the same function name.
-/**Return a String that represets the 2D array in the format:
-  * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
-  * Note the comma+space between values, and between arrays
-  * You are encouraged to notice that you may want to re-use
-  * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
-  */
-public static String arrToString(int[][]ary){
-  //this should use arrToString(int[])
-  String newString = "[";
-  for (int i = 0; i < ary.length; i++){
-    newString += arrToString(ary[i]);
-    if (i < ary.length - 1){
-      newString += ", ";
+  //3. Write arrToString, with a 2D array parameter.
+  //Note: Different parameters are allowed with the same function name.
+  /**Return a String that represets the 2D array in the format:
+    * "[[2, 3, 4], [5, 6, 7], [2, 4, 9]]"
+    * Note the comma+space between values, and between arrays
+    * You are encouraged to notice that you may want to re-use
+    * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
+    */
+  public static String arrToString(int[][]ary){
+    //this should use arrToString(int[])
+    String newString = "[";
+    for (int i = 0; i < ary.length; i++){
+      newString += arrToString(ary[i]);
+      if (i < ary.length - 1){
+        newString += ", ";
+      }
     }
+    return newString += "]";
   }
-  return newString += "]";
-}
 
-/*Return the sum of all of the values in the 2D array */
-public static int arr2DSum(int[][]nums){
-  //use a nested loop to solve this
-  int sum = 0;
-  for (int i = 0; i < nums.length; i++){
-    for (int x = 0; x < nums[i].length; x++){
-      sum += nums[i][x];
+  /*Return the sum of all of the values in the 2D array */
+  public static int arr2DSum(int[][]nums){
+    //use a nested loop to solve this
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++){
+      for (int x = 0; x < nums[i].length; x++){
+        sum += nums[i][x];
+      }
     }
+    return sum;
   }
-  return sum;
-}
 
-/**Rotate an array by returning a new array with the rows and columns swapped.
-  * You may assume the array is rectangular and neither rows nor cols is 0.
-  * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
-  */
-public static int[][] swapRC(int[][]nums){
-  int [][] newArr = new int [nums[0].length][nums.length];
-  for (int i = 0; i < nums.length; i++){
-    for (int x = 0; x < nums[i].length; x++){
-      newArr[x][i] = nums[i][x];
+  /**Rotate an array by returning a new array with the rows and columns swapped.
+    * You may assume the array is rectangular and neither rows nor cols is 0.
+    * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
+    */
+  public static int[][] swapRC(int[][]nums){
+    int [][] newArr = new int [nums[0].length][nums.length];
+    for (int i = 0; i < nums.length; i++){
+      for (int x = 0; x < nums[i].length; x++){
+        newArr[x][i] = nums[i][x];
+      }
     }
+    return newArr;
   }
-  return newArr;
-}
 }
