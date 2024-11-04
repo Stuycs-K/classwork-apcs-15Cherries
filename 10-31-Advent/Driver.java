e6d74707d53724126c17909f7c774b8e0f4933d6
Nowar <nowarnizum2008@icloud.com>
2024-11-04 09:59:26 -0500

    inputTri is being printed

---
+++ 10-31-Advent/Driver.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Driver{
+  /*File file = new File("Driver.java");
+  Scanner input = new Scanner(file);*/
+
+  public static void main (String[] args){
+
+    try {
+      File file = new File("inputTri.txt");
+      Scanner input = new Scanner(file);
+      while(input.hasNextLine()){
+        System.out.println(input.nextLine());
+      }
+        //do something with the next line of the file
+      //CODE THAT SCANS THE FILE.
+      input.close();//releases the file from your program
+
+      } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return; //you can return from a void function just don't put a value.
+      }
+
+
+    }
+}
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TriangleTester{
+  File file = new File("inputTri.txt");
+  Scanner input = new Scanner(file);
+  }
 
 
 }
