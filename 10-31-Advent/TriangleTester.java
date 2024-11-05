550b6ffb0648859e8c0416f98579549e7dd40b5d
Nowar <nowarnizum2008@icloud.com>
2024-11-05 14:55:31 -0500

    countTrianglesB written

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -9,10 +9,7 @@ public class TriangleTester{
     int sideA = Integer.parseInt(triSides[0]);
     int sideB = Integer.parseInt(triSides[1]);
     int sideC = Integer.parseInt(triSides[2]);
-    if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA){
-      return true;
-    }
-    return false;
+    return sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA;
   }
 
   public static int countTrianglesA(String fileName){
@@ -33,5 +30,57 @@ public class TriangleTester{
     return count;
   }
 
+  public static int countRows(String fileName){
+    int count = 0;
+    try {
+        File file = new File(fileName);
+        Scanner input = new Scanner(file);
+
+        while(input.hasNextLine()){
+          count++;
+        }
+        input.close();
+      } catch (FileNotFoundException ex) {
+        System.out.println("File not found");
+      }
+    return count;
+  }
+
+  public static int countTrianglesB(String fileName){
+    int count = 0;
+
+    try{
+      File file = new File(fileName);
+      Scanner input = new Scanner(file);
+
+      int rows = countRows(fileName);
+      int [][] fileVal = new int [rows][3];
+
+      int i = 0;
+      while (input.hasNextLine() && i < rows){
+        String triSides = input.nextLine().split.("\\s+");
+        for (int x = 0; x < 3; x++){
+          values[i][x] = Interger.parseInt(triSides[x]);
+        }
+        i++
+      }
+      input.close();
+
+      for (int x = 0; x < 3; x++){
+        for (int i = 0; i < rows; i+=3){
+          int sideA = fileVal[i][x];
+          int sideB = fileVal[i+1][x];
+          int sideC = fileVal[i+2][x];
+
+          if (isTriangle(sideA, sideB, sideC)){
+            count++
+          }
+        }
+      }
+    }catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    return count;
+  }
 
 }
