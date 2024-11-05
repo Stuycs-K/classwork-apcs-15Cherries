8a0955a1e130e7802d08dd5a8c83de9192306635
Nowar <nowarnizum2008@icloud.com>
2024-11-05 13:30:54 -0500

    countTrianglesA written and working

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -8,24 +8,7 @@ public class Driver{
 
   public static void main (String[] args){
 
-    try {
-      File file = new File("inputTri.txt");
-      Scanner input = new Scanner(file);
-      while(input.hasNextLine()){
-        System.out.println(input.nextLine());
-      }
-        //do something with the next line of the file
-      //CODE THAT SCANS THE FILE.
-      input.close();//releases the file from your program
-
-      } catch (FileNotFoundException ex) {
-      //File not found what should you do?
-      System.out.println("File not found");
-      return; //you can return from a void function just don't put a value.
-      }
-
-      System.out.println(TriangleTester.countTrianglesA(inputTri.txt));
-
+    System.out.println(TriangleTester.countTrianglesA("inputTri.txt"));
 
     }
 }
--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -5,38 +5,33 @@ import java.util.Scanner;
 public class TriangleTester{
 
   public static boolean isTriangle(String sides){
-    String [] triSides = sides.split(" ");
+    String [] triSides = sides.split("\\s+");
     int sideA = Integer.parseInt(triSides[0]);
     int sideB = Integer.parseInt(triSides[1]);
     int sideC = Integer.parseInt(triSides[2]);
-    if (sideA + sideB < sideC && sideA + sideC < sideB && sideB + sideC < sideA){
+    if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA){
       return true;
     }
     return false;
   }
 
-  /*public static int countTrianglesA(String fileName){
+  public static int countTrianglesA(String fileName){
+    int count = 0;
     try {
-        File file = new File("fileName");//1
+        File file = new File(fileName);
         Scanner input = new Scanner(file);
-        //CODE THAT SCANS THE FILE.
-        input.close();//releases the file from your program
 
+        while(input.hasNextLine()){
+          if (isTriangle (input.nextLine())){
+            count++;
+          }
+        }
+        input.close();
       } catch (FileNotFoundException ex) {
-        //File not found what should you do?
         System.out.println("File not found");
       }
-    int count = 0;
-    File file = new File(fileName);
-    Scanner input = new Scanner(file);
-    int [] sideA;
-    while(input.hasNextLine()){
-      System.out.println(input.nextLine());
-      sideA = (input.nextLine()).split(" ");
-      //System.out.println(sideA);
-    }
     return count;
-  }*/
+  }
 
 
 }
