d3980ea5f38ae88cb015c2aec84e8b37887e97f0
Nowar <nowarnizum2008@icloud.com>
2024-11-17 23:46:12 -0500

    replaceEmpty and makeReversedList written and tested

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -15,4 +15,31 @@ public class ArrayListPractice{
     }
     return randomArray;
   }
+
+  public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+    int arraySize = original.size();
+    for (int i = 0; i < arraySize; i++){
+      if (original.get(i).equals("")){
+        original.set(i, "Empty");
+      }
+    }
+  }
+
+  public static ArrayList<String> makeReversedList( ArrayList<String> original){
+  //return a new ArrayList that is in the reversed order of the original.
+    ArrayList<String> reversedArray = new ArrayList<String>();
+    for (int i = original.size() - 1; i >= 0; i--){
+      reversedArray.add(original.get(i));
+    }
+    return reversedArray;
+  }
+
+  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //If one list is longer than the other, just attach the remaining values to the end.
+  return a;
+  }
 }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -4,5 +4,8 @@ public class Driver{
   public static void main (String[] args){
     ArrayList<String> trial = ArrayListPractice.createRandomArray(5);
     System.out.println(trial);
+    ArrayListPractice.replaceEmpty(trial);
+    System.out.println(trial);
+    System.out.println(ArrayListPractice.makeReversedList(trial));
   }
 }
