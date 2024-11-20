675a0ab5253d4a81e6bbb1afefac30c362dd8ec5
Nowar <nowarnizum2008@icloud.com>
2024-11-19 20:35:24 -0500

    attempt to fix error with direction

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -18,32 +18,41 @@ public class Day1{
   public static int totalBlocks(String[] data){
     int x = 0;
     int y = 0;
+    int facing = 90;  //0 = North; 90 = East; 180 = South; 270 = West
     for (int i = 0; i < data.length; i++){
       String direction = data[i].substring(0,1);
-      int block = Integer.valueOf(data[i].substring(1));
-      int facing = 90;
-      if (direction.equals("R") && facing == 90){
-        x += block;
-        facing = 0;
+      int block = Integer.parseInt(data[i].substring(1));
+      if (direction.equals("R")){
+        facing = (facing + 90) % 360;
       }
-      if (direction.equals("R") && facing == 0){
-        y -= block;
-        facing = 90;
+      else if (direction.equals("L")){
+        facing = (facing + 270) % 360;
       }
-      if (direction.equals("L") && facing == 90){
-        x -= block;
-        facing = 0;
-      }
-      if (direction.equals("L") && facing == 0){
+      if (facing == 0){
         y += block;
-        facing = 90;
+        break;
+      }
+      if (facing == 90){
+        x += block;
+        break;
+      }
+      if (facing == 180){
+        y -= block;
+        break;
+      }
+      if (facing == 270){
+        x -= block;
+        break;
       }
     }
-    return 0;
+    return Math.abs(x) + Math.abs(y);
   }
 
   public static void main (String [] args){
     String [] data = parse("Day1Input.txt");
-    System.out.println(totalBlocks(data));
+    //String [] try = new String [] {"R2", "L3"};
+    System.out.println(totalBlocks(new String [] {"R2", "L3"}));
+    System.out.println(totalBlocks(new String [] {"R2", "R2", "R2"}));
+
   }
 }
