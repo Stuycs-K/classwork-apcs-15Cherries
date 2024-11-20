a02b9bc38c93518d63a8b9bee67cfe3f5bc35777
Nowar <nowarnizum2008@icloud.com>
2024-11-19 20:03:41 -0500

    4 way if statements for all directions written

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -7,7 +7,7 @@ public class Day1{
     try{
       Scanner inf = new Scanner(new File(fileName));
       String line = inf.nextLine();
-      System.out.print(line);
+      //System.out.print(line);
       return line.split(", ");
     } catch (Exception e){
       System.out.println("file not found");
@@ -19,9 +19,31 @@ public class Day1{
     int x = 0;
     int y = 0;
     for (int i = 0; i < data.length; i++){
-      String direction = data[i].substring(0);
+      String direction = data[i].substring(0,1);
       int block = Integer.valueOf(data[i].substring(1));
+      int facing = 90;
+      if (direction.equals("R") && facing == 90){
+        x += block;
+        facing = 0;
+      }
+      if (direction.equals("R") && facing == 0){
+        y -= block;
+        facing = 90;
+      }
+      if (direction.equals("L") && facing == 90){
+        x -= block;
+        facing = 0;
+      }
+      if (direction.equals("L") && facing == 0){
+        y += block;
+        facing = 90;
+      }
     }
     return 0;
+  }
+
+  public static void main (String [] args){
+    String [] data = parse("Day1Input.txt");
+    System.out.println(totalBlocks(data));
   }
 }
