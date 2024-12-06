66056d82243a1a921affc0e22ec712d6ba8f2b69
Nowar <nowarnizum2008@icloud.com>
2024-12-06 10:06:10 -0500

    accessor methods complete

--- 2024-12-06-Adventurer/Bard.java
+++ 2024-12-06-Adventurer/Bard.java
@@ -1,19 +1,52 @@
 public class Bard extends Adventurer{
-  private int age;
+  /*private int age;
   private String hometown;
-  private String instrument;
+  private String instrument;*/
+  private String SpecialName;
+  private int Special;
+  private int SpecialMax;
 
-  public Bard(String name, int age, String hometown, String instrument){
+  public Bard(String name){
     super(name);
-    this.age = age;
-    this.hometown = hometown;
-    this.instrument = instrument;
   }
 
-  public Bard(String name, int age, String hometown, String instrument, int hp){
+  public Bard(String name, int hp){
     super(name, hp);
-    this.age = age;
-    this.hometown = hometown;
-    this.instrument = instrument;
   }
+
+  public String getSpecialName(){
+    return SpecialName;
+  }
+
+  public int getSpecial(){
+    return Special;
+  }
+
+  public void setSpecial(int n){
+    Special = n;
+  }
+
+  public int getSpecialMax(){
+    return SpecialMax;
+  }
+
+  public String attack(Adventurer other){
+    return null;
+  }
+
+  //heall or buff the target adventurer
+  public String support(Adventurer other){
+    return null;
+  }
+
+  //heall or buff self
+  public String support(){
+    return null;
+  }
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public String specialAttack(Adventurer other){
+    return null;
+  }
+
 }
