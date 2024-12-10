01407a9bcfc03749f433ec99c0d900ad3052ada5
Nowar <nowarnizum2008@icloud.com>
2024-12-09 19:27:18 -0500

    loops through responses from both players

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Game{
+
+  public static void opponentMove(Adventurer bot, Adventurer other){
+    Random rand = new Random();
+    int chance = rand.nextInt(2)+1;
+    if (bot.getHP() < 4 && other.getHP() < 4 ){
+      if (chance == 1){
+        System.out.println(bot.support());
+      }
+      if (chance == 2){
+        System.out.println(bot.attack(other));
+      }
+    }
+    else if (other.getHP() > other.getmaxHP() / 2 && bot.getSpecial() > 7){
+      System.out.println(bot.specialAttack(other));
+    }
+    else{
+      System.out.println(bot.attack(other));
+    }
+  }
+
   public static void main(String[] args) {
     //do this once
     Scanner userInput = new Scanner(System.in);
@@ -18,16 +39,18 @@ public class Game{
     Adventurer player = new Bard(userName);
     Adventurer opponent = new CodeWarrior("Opponent");
 
-    System.out.println(player + ", " + player.getHP() + "/" + player.getmaxHP() + ", " + player.getSpecial() + "/" + player.getSpecialMax());
-    System.out.println(opponent + ", " + opponent.getHP() + "/" + opponent.getmaxHP() + ", " + opponent.getSpecial() + "/" + opponent.getSpecialMax());
+    //System.out.println(player + ", " + player.getHP() + "/" + player.getmaxHP() + ", " + player.getSpecial() + "/" + player.getSpecialMax());
+    //System.out.println(opponent + ", " + opponent.getHP() + "/" + opponent.getmaxHP() + ", " + opponent.getSpecial() + "/" + opponent.getSpecialMax());
 
 
     String input = "null";
     while (!input.equals("quit") && opponent.getHP() > 0 && player.getHP() > 0){
+      System.out.println("-------------------------------------------------");
       System.out.println(player + ", " + player.getHP() + "/" + player.getmaxHP() + ", " + player.getSpecial() + "/" + player.getSpecialMax());
       System.out.println(opponent + ", " + opponent.getHP() + "/" + opponent.getmaxHP() + ", " + opponent.getSpecial() + "/" + opponent.getSpecialMax());
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
       input = userInput.nextLine();
+
       if (! (input.equals("a") || input.equals("sp") || input.equals("su") || input.equals("quit"))){
         System.out.println("invalid input. please type again");
         break;
@@ -35,14 +58,15 @@ public class Game{
 
         if (input.equals("a")){
           System.out.println(player.attack(opponent));
+          opponentMove(opponent, player);
         }
         if (input.equals("sp")){
           System.out.println(player.specialAttack(opponent));
-          break;
+          opponentMove(opponent, player);
         }
         if (input.equals("su")){
           System.out.println(player.support());
-          break;
+          opponentMove(opponent, player);
         }
     }
 
