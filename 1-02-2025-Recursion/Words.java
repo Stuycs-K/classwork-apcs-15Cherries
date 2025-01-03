e95d2441ebcbcb38735ca75a05ed78c4ab805676
Nowar <nowarnizum2008@icloud.com>
2025-01-03 10:09:41 -0500

    function written

---
+++ 1-02-2025-Recursion/Words.java
@@ -0,0 +1,19 @@
+public class Words{
+
+  public static void makeWords (int remainingLetters, String result, String alphabet){
+    if (remainingLetters == 1){
+      for (int i = 0; i < alphabet.length(); i++){
+        System.out.println(result + alphabet.charAt(i));
+      }
+    }
+    else{
+
+      for (int i = 0; i < alphabet.length(); i++){
+        System.out.print(result + alphabet.charAt(i));
+        makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+      }
+
+    }
+  }
+
+}
